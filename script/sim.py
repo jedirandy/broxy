3bb58934e9a67cff0e6aed7f4aa0a5ba916b6a4d
Sheng <jedirandy@gmail.com>
2015-03-25 16:39:52 -0400

    add requests simulator script

---
+++ script/sim.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+import csv
+import random
+import subprocess
+import argparse
+
+# sample by weight
+def weighted_sample(weights):
+    rnd = random.random() * sum(weights)
+    for i, w in enumerate(weights):
+        rnd -= w
+        if rnd < 0:
+            return i
+
+# sample uniformly
+def uniform_sample(start, end):
+	return random.randint(start, end)
+
+# gaussian dist
+def gauss_sample(list):
+	mid = len(list)/2.0
+	stdev = (len(list) - mid)/3.0
+	result = int(random.gauss(mid,stdev))
+	if result < 0:
+		result = 0
+	if result >= len(list):
+		result = len(list) - 1
+	return result
+
+def run(program, host, input, output, num_samples, pattern):
+	result = []
+	with open(input, 'r') as f:
+		reader = csv.reader(f)
+		rows = list(reader)
+		# remove headers
+		rows = rows[1:]
+		if (pattern == 'weighted'):
+			weights = []
+			for i in range(1, len(rows) -1):
+				weights.append(rows[i][2])
+		if (pattern == 'gauss'):
+			# shuffle list
+			random.shuffle(rows)
+
+		# begin sampling
+		for i in range(1, num_samples + 1):
+			which = None
+			if (pattern == 'uniform'):
+				which = uniform_sample(0, len(rows) - 1)
+			elif (pattern == 'weighted'):
+				which = weighted_sample(weights)
+			elif (pattern == 'gauss'):
+				which = gauss_sample(rows)
+			rank = rows[which][0]
+			url = rows[which][1]
+			proc = subprocess.Popen([program, host, url], stdout=subprocess.PIPE)
+			time = proc.communicate()[0].split()[0]
+			result.append([rank, url, time])
+	with open(output, 'w') as f:
+		writer = csv.writer(f)
+		for r in result:
+			writer.writerow(r)
+	print output + ' generated'
+
+def main():
+	parser = argparse.ArgumentParser(description='script')
+	parser.add_argument('--host', dest='host', default='localhost', help='host')
+	parser.add_argument('--pattern', dest='pattern', default='uniform', help='requests pattern')
+	parser.add_argument('--samples', dest='samples', default=100, help='number of samples', type=int)
+	parser.add_argument('--input', dest='input', help='input file')
+	parser.add_argument('--output', dest='output', help='output file')
+	parser.add_argument('--program', dest='program', default='../client', help='program path')
+	args = parser.parse_args()
+
+	run(args.program, args.host, args.input, args.output, args.samples, args.pattern)
+
+if __name__ == '__main__':
+	main()
